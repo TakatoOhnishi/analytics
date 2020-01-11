80b666ffd303c3d959a36a1705a82f32eead5591
takato <freeney.freeney@gmail.com>
2020-01-11 16:06:47 +0900

    データ分析まわりで必要そうなスクリプト

---
+++ twitter/get_data/twitter_credentials.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#token取得方法は参考リンクを確認
+
+consumer_key= 'y4nZaj2LViYJRz9ELqJn5amzm'
+consumer_secret = 'X5WHLFzD1i6L5dmLlW9IZmkhlkTf1Q1rT2OvKXfVMKHCeCO7oo'
+
+Access_token  = '1173035845630038016-QurrcoPoay1DS1Vep8WMQyDRS0i03E'
+Access_secret = 'bPx5UaueuwUccdGQWx2e7NkXm81FRe5hpLTI0CXGypdTp'
+
